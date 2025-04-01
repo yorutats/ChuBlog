30ab94ee3dc85a3d0c8b0888446cc1be99095107
YiChu <s20009099@gmail.com>
2025-04-02 02:07:16 +0800

    chore: 加入預設圖像作為圖片載入失敗時的備援處理

--- src/Views/Portfolio/pages/Page4.js
+++ src/Views/Portfolio/pages/Page4.js
@@ -34,7 +34,7 @@ export default function Page4() {
                   imageSrc = require(`../../../images/${img.path}`);
                 } catch (e) {
                   console.warn("圖片載入失敗:", img.path);
-                  imageSrc = ""; // 或設定成預設圖像 require('../../../images/default.jpg')
+                  imageSrc = require("../../../images/default.png"); // 或設定成預設圖像
                 }
 
                 return (
